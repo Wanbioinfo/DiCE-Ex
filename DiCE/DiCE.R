15714fb628a7aea3469d2f6bf5255c19fa16b920
Sandali Lokuge <sdlokuge@iu.edu>
2026-03-06 10:30:26 -0500

    posit issue

--- DiCE/DiCE.R
+++ DiCE/DiCE.R
@@ -164,14 +164,12 @@ perform_DiCE <- function(
   # }
   
   if (tolower(species) == "human") {
-    string_protInfo_file <- file.path("extdata", "stringDB_v12", "human", "9606.protein.info.v12.0.txt")
-    string_ppi_file      <- file.path("extdata", "stringDB_v12", "human", "9606.protein.links.v12.0.txt")
-    tf_file              <- file.path("extdata", "TFs", "uniprotkb_keyword_KW_0805_AND_organism_human.xlsx")
+    string_protInfo_file <- "extdata/stringDB_v12/human/9606.protein.info.v12.0.txt"
+    string_ppi_file <- "extdata/stringDB_v12/human/9606.protein.links.v12.0.txt.gz"
     taxonID <- 9606
   } else if (tolower(species) == "mouse") {
-    string_protInfo_file <- file.path("extdata", "stringDB_v12", "mouse", "10090.protein.info.v12.0.txt")
-    string_ppi_file      <- file.path("extdata", "stringDB_v12", "mouse", "10090.protein.links.v12.0.txt")
-    tf_file              <- file.path("extdata", "TFs", "uniprotkb_keyword_KW_0805_AND_organism_mouse.xlsx")
+    string_protInfo_file <- "extdata/stringDB_v12/mouse/10090.protein.info.v12.0.txt"
+    string_ppi_file <- "extdata/stringDB_v12/mouse/10090.protein.links.v12.0.txt.gz"
     taxonID <- 10090
   }else{
     stop("Invalid species!. DiCE supports only for 'human' and 'mouse'")
